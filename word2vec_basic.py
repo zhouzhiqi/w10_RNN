62edc4123217699bfdcf5b72ab9c84f71f42d429
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-05-11 20:08:29 +0800

    指定不需要GUI的backend

--- word2vec_basic.py
+++ word2vec_basic.py
@@ -338,6 +338,7 @@ def plot_with_labels(low_dim_embs, labels, filename):
   assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
   plt.figure(figsize=(18, 18))  # in inches
   plt.rcParams['font.sans-serif'] = ['SimHei'] # 指定默认字体为黑体
+  plt.switch_backend('agg')
   for i, label in enumerate(labels):
     x, y = low_dim_embs[i, :]
     plt.scatter(x, y)
