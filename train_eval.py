6469c02df8bc317b3f4a7d1cf8610ef1857defbd
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-05-16 13:22:52 +0800

    更改环境编码为utf-8

--- train_eval.py
+++ train_eval.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import io
+import sys
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 
 from flags import parse_args
 
